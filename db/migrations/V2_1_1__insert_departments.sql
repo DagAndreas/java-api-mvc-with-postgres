c25b05047f4d6acb4c5d498b20155ce03af48bba
Dag Andreas <daanfofo@gmail.com>
2024-09-06 15:29:27 +0200

    added insert values to extention tables.

---
+++ db/migrations/V2_1_1__insert_departments.sql
@@ -0,0 +1,9 @@
+INSERT INTO departments (name, location)
+VALUES
+('Software Development', 'Manchester, UK'),
+('Human Resources', 'London, UK'),
+('Marketing', 'New York, USA'),
+('Sales', 'Berlin, Germany'),
+('Finance', 'Tokyo, Japan'),
+('Customer Support', 'Sydney, Australia'),
+('Research and Development', 'San Francisco, USA');
