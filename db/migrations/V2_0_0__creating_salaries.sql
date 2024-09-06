be1bcaa52e808e4c2f33066b60085adfe2f9dc5c
Dag Andreas <daanfofo@gmail.com>
2024-09-06 15:14:29 +0200

    created empty department and salaries tables.

---
+++ db/migrations/V2_0_0__creating_salaries.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS salaries;
+
+CREATE TABLE Salaries(
+    id TEXT PRIMARY KEY,
+    minSalary INT,
+    maxSalary INT
+);
---
+++ db/migrations/V2_1_0__creating_departments.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS Departments;
+
+CREATE TABLE Departments(
+    name TEXT PRIMARY KEY,
+    location TEXT
+);
