7c4b2d6b888ca5e60fd72d7b0deab1d19dca0b63
Dag Andreas <daanfofo@gmail.com>
2024-09-06 11:54:29 +0200

    flyway migrations

---
+++ db/migrations/V1_0_0__create_employees.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS Employees;
+
+CREATE TABLE Employees(
+     id SERIAL PRIMARY KEY,
+     name TEXT NOT NULL,
+     jobName TEXT NOT NULL,
+     salaryGrade TEXT NOT NULL,
+     department TEXT NOT NULL
+);
---
+++ db/migrations/V1_1_0__populate_employees.sql
@@ -0,0 +1,13 @@
+INSERT INTO Employees
+    (name, jobName, salaryGrade, department)
+VALUES
+('Albert Einstein', 'Theoretical Physicist', 'A1', 'Research & Development'),
+('Marie Curie', 'Radiation Scientist', 'A2', 'Medical Research'),
+('Leonardo da Vinci', 'Inventor & Artist', 'A3', 'Creative Design'),
+('Ada Lovelace', 'Mathematician & Programmer', 'A1', 'Information Technology'),
+('Nikola Tesla', 'Electrical Engineer', 'A2', 'Engineering'),
+('Jane Austen', 'Author', 'A3', 'Literature'),
+('Nelson Mandela', 'Human Rights Advocate', 'A4', 'Public Relations'),
+('Steve Jobs', 'CEO', 'A5', 'Management'),
+('Serena Williams', 'Professional Athlete', 'A3', 'Sports'),
+('Mahatma Gandhi', 'Civil Rights Leader', 'A4', 'Public Relations');
